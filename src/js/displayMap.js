b78559aef692d9c15ebd4a74c4dd62b0dea6ba19
Anusha Balusu <anusha.balusu08@gmail.com>
2017-02-25 16:01:59 -0500

    refactored the code and modified map interface

---
+++ src/js/displayMap.js
@@ -0,0 +1,15 @@
+
+      var map;
+
+      function initMap() {
+      	//alert("hi");
+        
+        map = new google.maps.Map(document.getElementById("map"), {
+          center: {lat: 43.085, lng: -77.676},
+          zoom: 16
+        });
+
+
+      }
+
+      
